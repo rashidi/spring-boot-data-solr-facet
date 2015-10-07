44174eb9b93a447c72bfbc5665445c2113acca9e
Mohd Rashidi Bin Mohd Zin <mohdrashidi.mohdzin@gfk.com>
2015-10-07 16:41:05 +0800

    Remove configuration for solr properties
    Since we are assigning default url. It is totally unnecessary

--- src/main/java/my/zin/rashidi/data/solr/Application.java
+++ src/main/java/my/zin/rashidi/data/solr/Application.java
@@ -1,21 +1,13 @@
 package my.zin.rashidi.data.solr;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.solr.SolrProperties;
 
 @SpringBootApplication
-public class Application implements CommandLineRunner {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @Override
-    public void run(String... strings) throws Exception {
-        SolrProperties solrProperties = new SolrProperties();
-        solrProperties.setHost("http://localhost:8983/solr");
     }
 
 }
