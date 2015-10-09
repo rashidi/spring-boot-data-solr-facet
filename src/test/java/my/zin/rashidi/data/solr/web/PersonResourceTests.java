c5a56c563d50508db165d14cabae3b3f38edccb9
Mohd Rashidi Bin Mohd Zin <mohdrashidi.mohdzin@gfk.com>
2015-10-09 16:33:17 +0800

    Add missing documentation for response

--- src/test/java/my/zin/rashidi/data/solr/web/PersonResourceTests.java
+++ src/test/java/my/zin/rashidi/data/solr/web/PersonResourceTests.java
@@ -19,6 +19,8 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -63,6 +65,10 @@ public class PersonResourceTests {
                 .andDo(document("person-example",
                         requestParameters(
                                 parameterWithName("name").description("Name of the person you are looking for")
+                        ),
+                        responseFields(
+                                fieldWithPath("name").description("Name of the person you are looking for"),
+                                fieldWithPath("location").description("Current location of the person")
                         )
                 ));
     }
